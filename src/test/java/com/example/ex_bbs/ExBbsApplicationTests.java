4d021fa9c490678e530168bd70051050db92790f
xxgbrdrgn <xxgbrdrgn@outlook.jp>
2026-05-27 13:50:20 +0900

    first commit

---
+++ src/test/java/com/example/ex_bbs/ExBbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ex_bbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExBbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
